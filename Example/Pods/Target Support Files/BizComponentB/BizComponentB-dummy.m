cfe6f31d2a8ff4663b5f7b921c71f5e22b88917e
任成 <rencheng11@icloud.com>
2022-08-29 15:09:40 +0800

    init project

---
+++ Example/Pods/Target Support Files/BizComponentB/BizComponentB-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_BizComponentB : NSObject
+@end
+@implementation PodsDummy_BizComponentB
+@end
---
+++ Example/Pods/Target Support Files/Pods-BizComponentB_Example/Pods-BizComponentB_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BizComponentB_Example : NSObject
+@end
+@implementation PodsDummy_Pods_BizComponentB_Example
+@end
