11541bb0c82cb804b9cfe1a580e3dbb5bce055fc
任成 <rencheng11@icloud.com>
2022-08-29 15:24:57 +0800

    init project

---
+++ BizComponentB/Classes/BizComponnetB.h
@@ -0,0 +1,16 @@
+//
+//  BizComponnetB.h
+//  AFNetworking
+//
+//  Created by 任成 on 2022/8/29.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BizComponnetB : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ BizComponentB/Classes/BizComponnetB.m
@@ -0,0 +1,12 @@
+//
+//  BizComponnetB.m
+//  AFNetworking
+//
+//  Created by 任成 on 2022/8/29.
+//
+
+#import "BizComponnetB.h"
+
+@implementation BizComponnetB
+
+@end
